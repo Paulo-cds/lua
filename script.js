f451f7dc9da7b8d6feef31c3d608636b5d92618d
paulo-cds <paulo.pdomingues@hotmail.com>
2021-07-16 17:30:19 -0300

    scroll

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menuMobile = document.querySelector('.menuMobile')
 let botaoMenu = document.querySelector('.botaoMenu')
-
+let goTop = document.querySelector('.goTop')
 
 let aberto = false
 
@@ -9,12 +9,12 @@ function abrirOuFecharMenu() {
     if (aberto === true){
         aberto = false
         menuMobile.classList.remove('aberto')
-        console.log(aberto)
+        
         
     } else {
         aberto = true
         menuMobile.classList.add('aberto')
-        console.log(aberto)
+        
     }
 }
 
@@ -36,3 +36,19 @@ function typeWrite(elemento){
 }
 const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
+
+
+
+
+
+window.addEventListener('scroll', function(){
+    if(this.window.pageYOffset > 350){
+        goTop.classList.add('active');
+    } else {
+        goTop.classList.remove('active');
+    }
+})
+
+goTop.onclick = function() {
+    window.scrollTo({top: 0,left: 0 , behavior: 'smooth'});
+}
